da91c133c57e10c7235dfbc5d0d4d552bdfae2ab
Priyank Agrawal <agrawal.pr@husky.neu.edu>
2016-11-16 22:07:14 -0500

    sorted stack program

---
+++ src/StacksAndQueues/SortStack.java
@@ -0,0 +1,47 @@
+package StacksAndQueues;
+
+import java.util.Stack;
+
+/**
+ * @author Priyank Agrawal
+ * 
+ * Sort a Stack using only one other Stack
+ * 
+ * Date: 11/16/2016
+ * 
+ * Space Complexity is O(N) and Time Complexity is O(N^2)
+ */
+public class SortStack {
+	
+	public static void main(String[] args){
+		Stack<Integer> stack = new Stack<>();
+		stack.push(10);
+		stack.push(11);
+		stack.push(12);
+		stack.push(8);
+		stack.push(9);
+		Stack<Integer> sorted = sort(stack);
+		int length = sorted.size();
+		System.out.println("Sorted Stack:");
+		for(int i=0; i<length;i++){
+			System.out.print(sorted.pop()+" ");
+		}
+	}
+	
+	/**
+	 * @param original stack
+	 * @return sorted stack
+	 */
+	public static Stack<Integer> sort(Stack<Integer> original){
+		Stack<Integer> sorted = new Stack<>();				//new sorted stack
+		while(!original.isEmpty()){
+			int temp = original.pop();						//top element of original stack stored in temporary variable						
+			while(!sorted.isEmpty() && sorted.peek()>temp){ //check whether top element of sorted stack is greater than top element of original stack
+				original.push(sorted.pop());				//push elements of sorted stack back onto the original stack
+			}
+			sorted.push(temp);								//push top element of original stack onto the sorted stack
+		}
+		return sorted;
+	}
+
+}
